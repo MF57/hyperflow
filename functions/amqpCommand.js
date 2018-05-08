903d22684fa0ee6dd28b81cbb62c24d133cda9e3
MF57 <p-bochenek@outlook.com>
2018-05-08 19:56:16 +0200

    aqmp command and index basic refactor

--- functions/amqpCommand.js
+++ functions/amqpCommand.js
@@ -1,90 +1,112 @@
-var uuid  = require('uuid');
-var when  = require('when');
-var defer = when.defer;
-var amqplib = require('amqplib');
-var executor_config = require('./config/amqpCommand.config.js');
+const uuid = require('uuid');
+const when = require('when');
+const defer = when.defer;
+const amqplib = require('amqplib');
+const executor_config = require('./config/amqpCommand.config.js');
 
-var identity = function(e) {return e};
+const identity = function (e) {
+    return e
+};
 
 
-var connection = null;
+let connection = null;
 
 function connect() {
     connection = amqplib.connect(executor_config.amqp_url);
     console.log("[AMQP] Starting connection to " + executor_config.amqp_url);
 
-    connection.then(function(conn) {
+    connection.then(function (conn) {
         console.log("[AMQP] Connected!");
 
-        return when(conn.createChannel().then(function(ch) {
-          var ok = ch.assertQueue('hyperflow.jobs', {durable: true}).then(function(qok) { return qok.queue; });
+        return when(conn.createChannel().then(function (ch) {
+            ch.assertQueue('hyperflow.jobs', {durable: true}).then(function (qok) {
+                return qok.queue;
+            });
         }));
-    }, function(err) {
+    }, function (err) {
         console.error('[AMQP] Connect failed: %s', err);
     })
 }
-var taskCount = 0;
+
+let taskCount = 0;
 
 function amqpCommand(ins, outs, config, cb) {
-  if(!connection) connect();
+    if (!connection) connect();
 
-  connection.then(function(connection) {
-    return when(connection.createChannel().then(function(ch) {
-      var options = executor_config.options;
-      if(config.executor.hasOwnProperty('options')) {
-          var executorOptions = config.executor.options;
-          for (var opt in executorOptions) {
-              if(executorOptions.hasOwnProperty(opt)) {
-                  options[opt] = executorOptions[opt];
-              }
-          }
-      }
-      var jobMessage = {
-        "executable": config.executor.executable,
-        "args":       config.executor.args,
-        "env":        (config.executor.env || {}),
-        "inputs":     ins.map(identity),
-        "outputs":    outs.map(identity),
-        "options":    options
-      };
+    connection.then(function (connection) {
+        return when(connection.createChannel().then(function (ch) {
+            const options = executor_config.options;
+            if (config.executor.hasOwnProperty('options')) {
+                const executorOptions = config.executor.options;
+                for (const opt in executorOptions) {
+                    if (executorOptions.hasOwnProperty(opt)) {
+                        options[opt] = executorOptions[opt];
+                    }
+                }
+            }
+            const jobMessage = {
+                "executable": config.executor.executable,
+                "args": config.executor.args,
+                "env": (config.executor.env || {}),
+                "inputs": ins.map(identity),
+                "outputs": outs.map(identity),
+                "options": options
+            };
 
-      var answer = defer();
-      var corrId = uuid.v4();
-      function maybeAnswer(msg) {
-        if (msg.properties.correlationId === corrId) {
-          answer.resolve(msg.content.toString());
-        }
-      }
+            const answer = defer();
+            const corrId = uuid.v4();
 
-      var ok = ch.assertQueue('', {exclusive: true, autoDelete: true})
-        .then(function(qok) { return qok.queue; });
+            function maybeAnswer(msg) {
+                console.log("MOZE ODPOWIEM");
+                if (msg.properties.correlationId === corrId) {
+                    answer.resolve(msg.content.toString());
+                }
+            }
 
-      ok = ok.then(function(queue) {
-        return ch.consume(queue, maybeAnswer, {noAck: true})
-          .then(function() { return queue; });
-      });
+            let ok = ch.assertQueue('', {exclusive: true, autoDelete: true})
+                .then(function (qok) {
+                    console.log("JESTEM W TYM MIEJSCU");
+                    return qok.queue;
+                });
 
-      ok = ok.then(function(queue) {
-        taskCount += 1;
-        // console.log("[AMQP][" + corrId + "][" + taskCount + "] Publishing job " + JSON.stringify(jobMessage));
-        ch.sendToQueue('hyperflow.jobs', new Buffer(JSON.stringify(jobMessage)), {replyTo: queue, contentType: 'application/json', correlationId: corrId});
-        return answer.promise;
-      });
+            ok = ok.then(function (queue) {
+                console.log(queue);
+                return ch.consume(queue, maybeAnswer, {noAck: true})
+                    .then(function () {
+                        console.log("A NIE BO W TYM");
+                        return queue;
+                    });
+            });
 
-      return ok.then(function(message) {
-        var parsed = JSON.parse(message);
-        ch.close();
-        if (parsed.exit_status == "0") {
-          // console.log("[AMQP][" + corrId + "] Job finished! job[" + JSON.stringify(jobMessage) + "] msg[" + message + "]", outs);
-          cb(null, outs);
-        } else {
-          // console.log("[AMQP][" + corrId + "] Error during job execution! msg[" + JSON.stringify(jobMessage) + "] job[" + message + "] exception[" + parsed.exceptions + "]");
-          // process.exit(5);
-          cb(parsed.exceptions, outs);
-        }
-      });
-    }))
-  }).then(null, function(err) { console.trace(err.stack); });
+            ok = ok.then(function (queue) {
+                taskCount += 1;
+               //  console.log("[AMQP][" + corrId + "][" + taskCount + "] Publishing job " + JSON.stringify(jobMessage));
+                ch.sendToQueue('hyperflow.jobs', new Buffer(JSON.stringify(jobMessage)), {
+                    replyTo: queue,
+                    contentType: 'application/json',
+                    correlationId: corrId
+                });
+                console.log("WYSLALEM");
+                return answer.promise;
+            });
+
+            return ok.then(function (message) {
+                const parsed = JSON.parse(message);
+                console.log(parsed);
+                ch.close();
+                if (parsed.exit_status === "0") {
+                    // console.log("[AMQP][" + corrId + "] Job finished! job[" + JSON.stringify(jobMessage) + "] msg[" + message + "]", outs);
+                    cb(null, outs);
+                } else {
+                   //  console.log("[AMQP][" + corrId + "] Error during job execution! msg[" + JSON.stringify(jobMessage) + "] job[" + message + "] exception[" + parsed.exceptions + "]");
+                    // process.exit(5);
+                    cb(parsed.exceptions, outs);
+                }
+            });
+        }))
+    }).then(null, function (err) {
+        console.trace(err.stack);
+    });
 }
 
 
--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,7 @@ const fsp = require('./fileSplitter.js'),
     scanDir = require('./DirScanner').scanDir;
 
 function print(ins, outs, config, cb) {
-    //console.log("PRINT", JSON.stringify(ins));
         ins.forEach(function(input) {
-                //console.log("sigId=", input.sigId + ":", input.data[0])
-        //console.log(JSON.stringify(input, null, 2));
         if (input.data && input.data[0].value) {
                     console.log(input.data[0].value);
         } else {
@@ -22,7 +19,7 @@ function print(ins, outs, config, cb) {
 function print2(ins, outs, config, cb) {
     console.log("PRINT2");
     ins.forEach(function(input) {
-        if (input.data.length == 1) {
+        if (input.data.length === 1) {
             console.log(input.data[0]);
         } else {
             console.log(input.data);
@@ -34,21 +31,12 @@ function print2(ins, outs, config, cb) {
 }
 
 function echo(ins, outs, config, cb) {
-    var data = JSON.stringify(ins[0].data);
-    //console.log(data);
+    JSON.stringify(ins[0].data);
     outs[0].data = [ins[0].data];
-    //onsole.log("ECHO", JSON.stringify(ins, null, 2));
-
-    //if (typeof data == "object" || typeof data == "array")
-     //   data = JSON.stringify(data);
-
-    //process.stdout.write(data[2]);
-    //console.log(data);
     cb(null, outs);
 }
 
 function echoWithDelay(ins, outs, config, cb) {
-    //console.log(ins, outs);
     outs[0].data = [ins[0].data];
     setTimeout(function() {
         cb(null, outs);
@@ -56,8 +44,8 @@ function echoWithDelay(ins, outs, config, cb) {
 }
 
 function add(ins, outs, config, cb) {
-    var sum=0.0;
-    for (var i=0; i<ins.length; ++i) {
+    let sum = 0.0;
+    for (let i=0; i<ins.length; ++i) {
         if ("value" in ins[i].data[0]) {
             sum += parseFloat(ins[i].data[0].value);
         }
@@ -70,7 +58,7 @@ function sqr(ins, outs, config, cb) {
     if (!("value" in ins[0].data[0])) {
         outs[0].data = [ new Error("functions:sqr : no input value provided") ];
     } else {
-        var v = parseFloat(ins[0].data[0].value);
+        const v = parseFloat(ins[0].data[0].value);
         outs[0].data[0] = { "value": v * v };
     }
     cb(null, outs);
@@ -88,12 +76,10 @@ function length(ins, outs, config, cb) {
 }
 
 function match(ins, outs, config, cb) {
-    var tmp = ins[0].data[0].value.match(new RegExp('^/(.*?)/(g?i?m?y?)$'));
-    var regex = new RegExp(tmp[1], tmp[2]);
-    var str = ins[1].data[0].value;
-    var s = str.search(regex);
-    //console.log(regex, str, s);
-    if (str.search(regex) != -1) {
+    const tmp = ins[0].data[0].value.match(new RegExp('^/(.*?)/(g?i?m?y?)$'));
+    const regex = new RegExp(tmp[1], tmp[2]);
+    const str = ins[1].data[0].value;
+    if (str.search(regex) !== -1) {
         outs[0].condition = "true";
         outs[0].data = [ str ];
     }
@@ -101,14 +87,13 @@ function match(ins, outs, config, cb) {
 }
 
 function chooseEvenOdd(ins, outs, config, cb) {
-    var sum=0;
-    //console.log("choose INS=", ins);
-    for (var i=0; i<ins.length; ++i) {
+    let sum = 0;
+    for (let i=0; i<ins.length; ++i) {
         if ("data" in ins[i] && "value" in ins[i].data[0]) {
             sum += parseInt(ins[i].data[0].value);
         }
     }
-    if (sum % 2 == 0) {
+    if (sum % 2 === 0) {
         outs[0].data = [ { "value": sum } ];
         outs[0].condition = "true";
     } else {
@@ -119,15 +104,14 @@ function chooseEvenOdd(ins, outs, config, cb) {
 }
 
 function scanDirForJs(ins, outs, config, cb) {
-    var inPath = ins[0].value, outPath;
+    let inPath = ins[0].value, outPath;
     if (outs[0].path) {
         outPath = outs[0].path;
     } else {
         outPath = inPath + "/" + "matchingFilesOut.txt";
         outs[0].path = outPath;
-        //outs[0].value = outPath;
     }
-    scanDir(inPath, /.*js$/, outPath, function(err, result) {
+    scanDir(inPath, /.*js$/, outPath, function(err) {
         err ? cb(err): cb(null, outs);
     });
 }
@@ -146,44 +130,30 @@ function grepFile(ins, outs, config, cb) {
     cb(null, outs); 
 }
 
-
-//var cnt = 0;
 function count(ins, outs, config, cb) {
-    //console.log("COUNT  INS:", JSON.stringify(ins));
-    //onsole.log(ins.length);
-    //onsole.log("COUNT INS:", ins.length);
-    /*ins.forEach(function(input) {
-      console.log(input); 
-    });*/
-    /*console.log("COUNT OUTS:", outs.length);
-    outs.forEach(function(output) {
-      console.log(output); 
-    });*/
- 
     outs[0].data = [];
     ins[0].data.forEach(function(cnt) {
         outs[0].data.push(cnt+1);
-        if (cnt % 1000 == 0) { 
+        if (cnt % 1000 === 0) {
             console.log("count:", cnt);
         }
-        if (cnt == 5000) {
+        if (cnt === 5000) {
             process.exit();
         }
     });
     cb(null, outs);
 }
 
-function exit(ins, outs, config, cb) {
+function exit() {
   console.log("Exiting\n\n");
   process.exit(0);
 }
 
 function genCollection(ins, outs, config, cb) {
-    var len = ins[0].data[0];
+    const len = ins[0].data[0];
     outs[0].data = [];
 
-    for (var i=0; i<len; i++) {
-        //outs[0].data.push(Math.floor(Math.random() * 5) + 1); 
+    for (let i=0; i<len; i++) {
         outs[0].data.push(i+1);
     }
 
@@ -195,14 +165,6 @@ function genCollection(ins, outs, config, cb) {
 function noop(ins, outs, config, cb) {
     cb(null, outs);
 }
-
-/*
-function montage_mProjectPP(ins, outs, config, cb) {
-    var execName = "mProjectPP";
-    var execArgs = "-X -x "+config.f.scale+" "+ins[0].name+" "+outs[0].name+" "+ins[1].name;
-    // invoke executor(execName, exsecArgs)
-}
-*/
 
 exports.print = print;
 exports.print2 = print2;
